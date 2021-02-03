c99fa09a64ae41265117a4d19b3c7c963eb00a9f
Brody Bayley <brodybayley@gmail.com>
2021-02-03 03:11:46 +0000

    function to check if arrays match

--- lotide/eqArrays.js
+++ lotide/eqArrays.js
@@ -16,7 +16,7 @@ const eqArrays = function (array1, array2) {
     }
   }
   return true;
-}
+};
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
