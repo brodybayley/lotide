a8a114ebec13c23febc109d799ef004cfefc5ec4
Brody Bayley <brodybayley@gmail.com>
2021-02-04 05:32:14 +0000

    submitting with final changes

--- lotide/eqObject.js
+++ lotide/eqObject.js
@@ -1,30 +1,39 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`❤️❤️❤️ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🤷🏻‍♂️🤷🏻‍♂️🤷🏻‍♂️ Assertion Failed: ${actual} !== ${expected}`);
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
   }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
-//worked with Elodie and Brody 
-const eqObjects = function (object1, object2) {
-  const object1Keys = Object.keys(object1)
-  const object2Keys = Object.keys(object2)
+//worked with Elodie and Brody
+const eqObjects = function(object1, object2) {
+  const object1Keys = Object.keys(object1);
+  const object2Keys = Object.keys(object2);
   console.log(object1Keys);
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
   for (let values of object1Keys) {
-    console.log(object1[values])
-    console.log(object1Keys[values])
     if (Array.isArray(object1Keys[values]) && Array.isArray(object2Keys[values])) {
-      if (!eqArrays(object1Keys, object2Keys))
+      if (!eqArrays(object1Keys, object2Keys)) {
         return false;
-    } else
-      return true;
+      } else {
+        if (object1Keys !== object2Keys) {
+          return false;
+        }
+      }
+    }
   }
+  return true;
+};
 
-  const ab = { a: "1", b: "2" };
-  const ba = { b: "2", a: "1" };
-  console.log(eqObjects(ab, ba));
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+console.log(eqObjects(ab, ba));
 
