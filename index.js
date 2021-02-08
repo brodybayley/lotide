d2b6da1388f2bf68fbd2f00f1191cb873df699a2
Brody Bayley <brodybayley@gmail.com>
2021-02-08 22:55:33 +0000

    pushing through final changes with all files add

--- index.js
+++ index.js
@@ -2,9 +2,29 @@ const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
 const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObject = require('./eqObject');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
 
 module.exports = {
   head: head,
   tail: tail,
-  middle: middle
+  middle: middle,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObject: eqObject,
+  findKeyByValue: findKeyByValue,
+  findKey: findKey,
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without
 };
