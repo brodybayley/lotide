8461f68ca817dea6eb71f27b94f3f9adb89c874c
Brody Bayley <brodybayley@gmail.com>
2021-02-08 21:20:05 +0000

    created test file for assertArraysEqual and cleaned up AssertArraysEqual file

---
+++ test/assertArraysEqual.test.js
@@ -0,0 +1,7 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const eqArrays = require('../eqArrays');
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
--- test/eqArrays.test.js
+++ test/eqArrays.test.js
@@ -1,5 +1,6 @@
 const eqArrays = require('../eqArrays');
 const assertEqual = require('../assertEqual');
+
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
