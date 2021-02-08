32ea8f442a04a7b56048ce1ba84e44093c5ed56f
Brody Bayley <brodybayley@gmail.com>
2021-02-08 22:29:50 +0000

    pushing through files that are added to library

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
+const countLetters = require('./countLetters');
 
 module.exports = {
   head: head,
